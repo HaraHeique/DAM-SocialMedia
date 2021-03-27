1ad4a038706e4d4462b1642b2d3551c9e07e1808
Harã Heique dos Santos <heikacademicos@hotmail.com>
2021-03-27 15:24:20 -0300

    fix: ajustes de layout e de repetições de notificações

--- src/app/src/main/java/com/example/socialmedia/Services/CloudMessageService.java
+++ src/app/src/main/java/com/example/socialmedia/Services/CloudMessageService.java
@@ -2,6 +2,7 @@ package com.example.socialmedia.Services;
 
 import android.app.Notification;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 
 import androidx.annotation.NonNull;
@@ -18,11 +19,20 @@ import java.util.Map;
 
 public class CloudMessageService extends FirebaseMessagingService {
 
+    private Context context;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        context = this;
+    }
+
     @Override
     public void onNewToken(@NonNull String s) {
         super.onNewToken(s);
 
-        AppConfig.setAppToken(this, s);
+        AppConfig.setAppToken(context, s);
     }
 
     @Override
@@ -38,27 +48,29 @@ public class CloudMessageService extends FirebaseMessagingService {
 
     private void notifyPostComment(String postId, String userName) {
         Intent intent = createIntentPostComment(postId);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
-        String notificationMessage = String.format("%s comentou o seu post.", userName);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        String notificationTitle = "Social Media";
+        String notificationText = String.format("%s comentou o seu post.", userName);
 
-        Notification notification = buildNotificationPostComment(pendingIntent, notificationMessage);
+        Notification notification = buildNotificationPostComment(pendingIntent, notificationTitle, notificationText);
 
-        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(this);
-        notificationManagerCompat.notify(AppConfig.nextNotificationId(this), notification);
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(context);
+        notificationManagerCompat.notify(AppConfig.nextNotificationId(context), notification);
     }
 
     private Intent createIntentPostComment(String id) {
-        Intent intent = new Intent(this, CommentActivity.class);
+        Intent intent = new Intent(context, CommentActivity.class);
         intent.putExtra("postId", id);
 
         return intent;
     }
 
-    private Notification buildNotificationPostComment(PendingIntent pendingIntent, String message) {
+    private Notification buildNotificationPostComment(PendingIntent pendingIntent, String title, String text) {
         NotificationCompat.Builder builder = new NotificationCompat
-            .Builder(this, getString(R.string.channel_id))
+            .Builder(context, getString(R.string.channel_id))
             .setSmallIcon(R.drawable.ic_round_message_24)
-            .setContentText(message)
+            .setContentTitle(title)
+            .setContentText(text)
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
             .setContentIntent(pendingIntent)
             .setAutoCancel(true);
